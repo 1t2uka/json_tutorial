3fbd2e058e5b4c39531307aa6a5498e01f6bd3d5
1t2uka <mateur@qq.com>
2025-11-27 15:51:35 +0800

    添加string,number,null,true和false的get-set函数测试单元，以及部分string及其错误类型测试单元.

--- test.c
+++ test.c
@@ -22,34 +22,67 @@ static int test_pass = 0;   //通过测试数量
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 #define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
 
+//改宏判断expect和actual的长度是否一致，并且逐字节比较expect和actual内存中的内容是否一致，且不以'\0'结束
+#define EXPECT_EQ_STRING(expect, actual, alength)\
+    EXPECT_EQ_BASE(sizeof(expect) - 1 == (alength) && memcmp(expect, actual, alength) == 0, expect, actual, "%s")
+
+#define EXPECT_EQ_TRUE(actual) EXPECT_EQ_BASE((actual) != 0, "true", "false", "%s")
+#define EXPECT_EQ_FALSE(actual) EXPECT_EQ_BASE((actual) == 0, "false", "true", "%s")
+
 #define TEST_ERROR(error, json)\
     do{\
         leptp_value v;\
+        leptp_init(&v);\
         v.type = LEPTP_FALSE;\
         EXPECT_EQ_INT(error, leptp_parse(&v, json));\
         EXPECT_EQ_INT(LEPTP_NULL, leptp_get_type(&v));\
+        leptp_free(&v);\
     }while(0)
 
 #define TEST_LITERAL(literal, json)\
     do{\
         leptp_value v;\
+        leptp_init(&v);\
+        leptp_set_boolean(&v,0);\
         EXPECT_EQ_INT(LEPTP_PARSE_OK, leptp_parse(&v,json));\
         EXPECT_EQ_INT(literal, leptp_get_type(&v));\
+        leptp_free(&v);\
     }while(0)
 
 #define TEST_NUMBER(expect, json)\
     do{\
         leptp_value v;\
+        leptp_init(&v);\
         EXPECT_EQ_INT(LEPTP_PARSE_OK, leptp_parse(&v, json));\
         EXPECT_EQ_INT(LEPTP_NUMBER, leptp_get_type(&v));\
         EXPECT_EQ_DOUBLE(expect, leptp_get_number(&v));\
+        leptp_free(&v);\
     }while(0);
+
+#define TEST_STRING(expect,json)\
+    do{\
+        leptp_value v;\
+        leptp_init(&v);\
+        EXPECT_EQ_INT(LEPTP_PARSE_OK, leptp_parse(&v, json));\
+        EXPECT_EQ_INT(LEPTP_STRING, leptp_get_type(&v));\
+        EXPECT_EQ_STRING(expect, leptp_get_string(&v), leptp_get_string_length(&v));\
+        leptp_free(&v);\
+    }while(0)
 
 //测试对null字面值解析是否正确
 static void test_parse_literal() {
     TEST_LITERAL(LEPTP_NULL, "null");
     TEST_LITERAL(LEPTP_TRUE, "true");
     TEST_LITERAL(LEPTP_FALSE, "false");
+}
+
+static void test_parse_string() {
+    TEST_STRING("","\"\"");
+    TEST_STRING("Hello","\"Hello\"");
+#if 0
+    TEST_STRING("Hello\nWorld","Hello\\nWorld\"");
+    TEST_STRING("\" \\ / \b \f \n \r \t","\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+#endif
 }
 
 static void test_parse_expect_value(){
@@ -119,13 +152,89 @@ static void test_number_too_big() {
 
 }
 
+//缺失引号
+static void test_parse_missing_quotation_mark() {
+    TEST_ERROR(LEPTP_PARSE_MISS_QUOTATION_MARK,"\"");
+    TEST_ERROR(LEPTP_PARSE_MISS_QUOTATION_MARK, "\"abc");
+}
+
+//无效转义字符
+static void test_parse_invalie_string_escape() {
+#if 0
+    TEST_ERROR(LEPTP_PARSE_INVALID_STRING_ESCAPE, "\"\\v\"");
+    TEST_ERROR(LEPTP_PARSE_INVALID_STRING_ESCAPE, "\"\\'\"");
+    TEST_ERROR(LEPTP_PARSE_INVALID_STRING_ESCAPE, "\"\\0\"");
+    TEST_ERROR(LEPTP_PARSE_INVALID_STRING_ESCAPE, "\"\\x12\"");
+#endif
+}
+
+//无效字符串字符
+static void test_parse_invalid_string_char() {
+#if 0
+    TEST_ERROR(LEPTP_PARSE_INVALID_STRING_CHAR, "\"\x01\"");
+    TEST_ERROR(LEPTP_PARSE_INVALID_STRING_CHAR, "\"\x1F\"");
+#endif
+}
+
+static void test_access_null(){
+    leptp_value v;
+    leptp_init(&v);
+    leptp_set_string(&v,"a",1);
+    leptp_set_null(&v);
+    EXPECT_EQ_INT(LEPTP_NULL, leptp_get_type(&v));
+    leptp_free(&v);
+}
+
+static void test_access_boolean() {
+    leptp_value v;
+    leptp_init(&v);
+    leptp_set_string(&v,"n",1);
+    leptp_set_boolean(&v,0);//LEPTP_FALSE != 0,此处传入LEPTP_FALSE时会被误认为LEPTP_FALSE > 0 == 1;
+    EXPECT_EQ_FALSE(leptp_get_boolean(&v));
+    leptp_set_null(&v);
+    leptp_set_boolean(&v,1);
+    EXPECT_EQ_TRUE(leptp_get_boolean(&v));
+    leptp_free(&v);
+}
+
+static void test_access_number() {
+    leptp_value v;
+    leptp_init(&v);
+    leptp_set_string(&v,"u",1);
+    leptp_set_number(&v,3.1);
+    EXPECT_EQ_DOUBLE(3.1,leptp_get_number(&v));
+    leptp_free(&v);
+}
+//string的get-set等函数测试单元
+static void test_access_string() {
+    leptp_value v;
+    leptp_init(&v); //set v->type = null;
+    leptp_set_string(&v,"",0);
+    EXPECT_EQ_STRING("",leptp_get_string(&v), leptp_get_string_length(&v));
+    leptp_set_string(&v, "Hello",5);
+    EXPECT_EQ_STRING("Hello", leptp_get_string(&v), leptp_get_string_length(&v));
+    leptp_free(&v);
+}
+
 static void test_parse(){
     test_parse_literal();
+    test_parse_number();
+    //test_parse_string();
+
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
-    test_parse_number();
+
     test_number_too_big();
+
+    test_parse_missing_quotation_mark();
+    test_parse_invalie_string_escape();
+    test_parse_invalid_string_char();
+
+    test_access_null();
+    test_access_boolean();
+    test_access_number();
+    test_access_string();
 }
 
 int main(){
