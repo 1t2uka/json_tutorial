41e41a2c7bdce0b624e6164c44e5778d578009d2
1t2uka <mateur@qq.com>
2025-12-02 11:42:42 +0800

    添加unicode转义序列测试单元，重构null,boolean,number的set-get函数

--- test.c
+++ test.c
@@ -79,8 +79,15 @@ static void test_parse_literal() {
 static void test_parse_string() {
     TEST_STRING("", "\"\"");
     TEST_STRING("Hello", "\"Hello\"");
-    TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");          /* <- 修复：加上起始/结束引号并转义 \n */
-    TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\""); /* 保持转义正确 */
+    TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
+    TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+    TEST_STRING("Hello\0World", "\"Hello\\u0000World\"");
+    TEST_STRING("\x24", "\"\\u0024\"");         /* Dollar sign U+0024 */
+    TEST_STRING("\xC2\xA2", "\"\\u00A2\"");     /* Cents sign U+00A2 */
+    TEST_STRING("\xE2\x82\xAC", "\"\\u20AC\""); /* Euro sign U+20AC */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\uD834\\uDD1E\"");  /* G clef sign U+1D11E */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\ud834\\udd1e\"");  /* G clef sign U+1D11E */
+
 }
 
 static void test_parse_expect_value(){
@@ -225,6 +232,9 @@ static void test_parse(){
     test_parse_invalie_string_escape();
     test_parse_invalid_string_char();
 
+}
+
+static void test_access() {
     test_access_null();
     test_access_boolean();
     test_access_number();
@@ -233,6 +243,7 @@ static void test_parse(){
 
 int main(){
     test_parse();
+    test_access();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
 }
