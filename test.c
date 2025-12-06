a36ab63a607c8f4c44cbbb7c0d5d4385a81ae5eb
1t2uka <mateur@qq.com>
2025-12-06 17:35:17 +0800

    添加对象测试单元，以及对象错误类型测试案例

--- test.c
+++ test.c
@@ -137,6 +137,65 @@ static void test_parse_array() {
     leptp_free(&v);
 }
 
+static void test_parse_object() {
+    leptp_value v;
+    size_t i;
+
+    leptp_init(&v);
+    EXPECT_EQ_INT(LEPTP_PARSE_OK, leptp_parse(&v, " {  } "));
+    EXPECT_EQ_INT(LEPTP_OBJECT, leptp_get_type(&v));
+    EXPECT_EQ_SIZE_T(0, leptp_get_object_size(&v));
+    leptp_free(&v);
+
+    leptp_init(&v);
+    EXPECT_EQ_INT(LEPTP_PARSE_OK, leptp_parse(&v, 
+        " { "
+        "\"n\" : null , "
+        "\"f\" : false , "
+        "\"t\" : true , "
+        "\"i\" : 123 , "
+        "\"s\" : \"abc\", "
+        "\"a\" : [ 1, 2, 3 ],"
+        "\"o\" : { \"1\" : 1, \"2\" : 2, \"3\" : 3 }"
+        " } "));
+    EXPECT_EQ_INT(LEPTP_OBJECT, leptp_get_type(&v));
+    EXPECT_EQ_SIZE_T(7, leptp_get_object_size(&v));
+    EXPECT_EQ_STRING("n", leptp_get_object_key(&v, 0), leptp_get_object_key_length(&v, 0));
+    EXPECT_EQ_INT(LEPTP_NULL,   leptp_get_type(leptp_get_object_value(&v, 0)));
+    EXPECT_EQ_STRING("f", leptp_get_object_key(&v, 1), leptp_get_object_key_length(&v, 1));
+    EXPECT_EQ_INT(LEPTP_FALSE,  leptp_get_type(leptp_get_object_value(&v, 1)));
+    EXPECT_EQ_STRING("t", leptp_get_object_key(&v, 2), leptp_get_object_key_length(&v, 2));
+    EXPECT_EQ_INT(LEPTP_TRUE,   leptp_get_type(leptp_get_object_value(&v, 2)));
+    EXPECT_EQ_STRING("i", leptp_get_object_key(&v, 3), leptp_get_object_key_length(&v, 3));
+    EXPECT_EQ_INT(LEPTP_NUMBER, leptp_get_type(leptp_get_object_value(&v, 3)));
+    EXPECT_EQ_DOUBLE(123.0, leptp_get_number(leptp_get_object_value(&v, 3)));
+    EXPECT_EQ_STRING("s", leptp_get_object_key(&v, 4), leptp_get_object_key_length(&v, 4));
+    EXPECT_EQ_INT(LEPTP_STRING, leptp_get_type(leptp_get_object_value(&v, 4)));
+    EXPECT_EQ_STRING("abc", leptp_get_string(leptp_get_object_value(&v, 4)), leptp_get_string_length(leptp_get_object_value(&v, 4)));
+    EXPECT_EQ_STRING("a", leptp_get_object_key(&v, 5), leptp_get_object_key_length(&v, 5));
+    EXPECT_EQ_INT(LEPTP_ARRAY, leptp_get_type(leptp_get_object_value(&v, 5)));
+    EXPECT_EQ_SIZE_T(3, leptp_get_array_size(leptp_get_object_value(&v, 5)));
+
+    for (i = 0; i < 3; i++) {
+        leptp_value* e = leptp_get_array_element(leptp_get_object_value(&v, 5), i);
+        EXPECT_EQ_INT(LEPTP_NUMBER, leptp_get_type(e));
+        EXPECT_EQ_DOUBLE(i + 1.0, leptp_get_number(e));
+    }
+    EXPECT_EQ_STRING("o", leptp_get_object_key(&v, 6), leptp_get_object_key_length(&v, 6));
+    {
+        leptp_value* o = leptp_get_object_value(&v, 6);
+        EXPECT_EQ_INT(LEPTP_OBJECT, leptp_get_type(o));
+        for (i = 0; i < 3; i++) {
+            leptp_value* ov = leptp_get_object_value(o, i);
+            EXPECT_EQ_TRUE('1' + i == leptp_get_object_key(o, i)[0]);
+            EXPECT_EQ_SIZE_T(1, leptp_get_object_key_length(o, i));
+            EXPECT_EQ_INT(LEPTP_NUMBER, leptp_get_type(ov));
+            EXPECT_EQ_DOUBLE(i + 1.0, leptp_get_number(ov));
+        }
+    }
+    leptp_free(&v);
+}
+
 static void test_parse_expect_value(){
     TEST_ERROR(LEPTP_PARSE_EXPECT_VALUE, "");
     TEST_ERROR(LEPTP_PARSE_EXPECT_VALUE, " ");
@@ -254,13 +313,35 @@ static void test_parse_invalid_unicode_surrogate() {
 }
 
 static void test_parse_miss_comma_or_square_bracket() {
-#if 0
     TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_SQUARE_BRACKET, "[1");
     TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_SQUARE_BRACKET, "[1}");
     TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_SQUARE_BRACKET, "[1 2");
     TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_SQUARE_BRACKET, "[[]");
-#endif
 }
+
+static void test_parse_miss_key() {
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{1:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{true:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{false:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{null:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{[]:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{{}:1,");
+    TEST_ERROR(LEPTP_PARSE_MISS_KEY, "{\"a\":1,");
+}
+
+static void test_parse_miss_colon() {
+    TEST_ERROR(LEPTP_PARSE_MISS_COLON, "{\"a\"}");
+    TEST_ERROR(LEPTP_PARSE_MISS_COLON, "{\"a\",\"b\"}");
+}
+
+static void test_parse_miss_comma_or_curly_bracket() {
+    TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":1");
+    TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":1]");
+    TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":1 \"b\"");
+    TEST_ERROR(LEPTP_PARSE_MISS_COMMA_OR_CURLY_BRACKET, "{\"a\":{}");
+}
+
 static void test_access_null(){
     leptp_value v;
     leptp_init(&v);
@@ -306,6 +387,7 @@ static void test_parse(){
     test_parse_number();
     test_parse_string();
     test_parse_array();
+    test_parse_object();
 
     test_parse_expect_value();
     test_parse_invalid_value();
@@ -320,8 +402,9 @@ static void test_parse(){
     test_parse_invalid_unicode_surrogate();
 
     test_parse_miss_comma_or_square_bracket();
-
-
+    test_parse_miss_key();
+    test_parse_miss_colon();
+    test_parse_miss_comma_or_curly_bracket();
 }
 
 static void test_access() {
